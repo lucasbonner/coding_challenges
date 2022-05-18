5f02b8cc42c04db728efa2dafc23d0b82a42abb0
Lucas <lucasbonner@protonmail.com>
2022-05-18 11:27:26 -0500

    refamiliarizing myself with ruby with easy problems, completes reversed_sequence

---
+++ ruby/codewars_leetcode/reversed_sequence.rb
@@ -0,0 +1,12 @@
+=begin
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+=end
+
+def reverse_seq(n)
+  #your code here
+end
+
+Test.assert_equals(reverse_seq(5),[5,4,3,2,1])
+Test.assert_equals(reverse_seq(6),[6,5,4,3,2,1])
