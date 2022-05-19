7470bb4343300c4ee03ea39eef63448d29857bab
Lucas <lucasbonner@protonmail.com>
2022-05-18 19:38:56 -0500

    first solution for two_sum.rb

--- ruby/grind_75/two_sum.rb
+++ ruby/grind_75/two_sum.rb
@@ -32,9 +32,18 @@ You can return the answer in any order.
 
 examples:
 
-Input: nums = [2,7,11,15], target = 9
+Input:
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+nums = [2,7,11,15], target = 9
+start iterating thru array
+1st loop is 0 -> array.length (i)
+  2nd loop is i -> array.length (j)
+    if arr at i + j === target, return in arr format
+
+
+
 Example 2:
 
 Input: nums = [3,2,4], target = 6
@@ -45,12 +54,22 @@ Input: nums = [3,3], target = 6
 Output: [0,1]
 
 pseudo/algo:
+need better efficiency
+how not iterate over array twice?
 
 =end
-def two_sum(nums, target)
-
+def two_sums(nums, target)
+  nums.select! { |num| num <= target }
+  for i in (0...nums.length) do
+    for j in (i + 1...nums.length) do
+      if nums[i] + nums[j] == target
+        return [i, j]
+      end
+    end
+  end
 end
 
-p two_sums([2,7,11,15], 9) #[0,1]
-p two_sums([3, 2, 4], 6) #[1, 2]
-p two_sums([3, 3], 6) #[0, 1]
+# p two_sums([2,7,11,15], 9) #[0,1]
+# p two_sums([3, 2, 4], 6) #[1, 2]
+# p two_sums([3, 3], 6) #[0, 1]
+p two_sums([0,4,3,0], 0)
