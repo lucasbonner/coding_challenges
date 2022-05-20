52cb2582b6e10f5221c9245b56249378fabe94a2
Lucas <lucasbonner@protonmail.com>
2022-05-20 14:57:56 -0500

    adds first attempt at valid_parentheses javascript

--- javascript/blind_75/valid_parentheses/valid_parentheses.js
+++ javascript/blind_75/valid_parentheses/valid_parentheses.js
@@ -1,5 +1,26 @@
 var isValid = function(s) {
+  let stack = [];
+  const associater = {
+    '(' : ')',
+    '[' : ']',
+    '{' : '}',
+  }
 
+  for (let i = 0; i < s.length; i++) {
+    if (Object.keys(associater).includes(s[i])) {
+      stack.push(s[i]);
+      continue;
+    }
+
+    let stackLastElement = stack[ stack.length - 1 ]
+    if (associater[stackLastElement] === s[i]) {
+      stack.pop();
+      continue;
+    }
+    return false;
+  }
+
+  return stack.length === 0;
 };
 
 console.log(isValid("()")) //true
