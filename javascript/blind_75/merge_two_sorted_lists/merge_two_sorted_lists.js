190acd7cf56fa92dbe9aaaa4e41aafd69efb40d1
Lucas <lucasbonner@protonmail.com>
2022-05-21 15:45:05 -0500

    adds merge two sorted lists js completed

--- javascript/blind_75/merge_two_sorted_lists/merge_two_sorted_lists.js
+++ javascript/blind_75/merge_two_sorted_lists/merge_two_sorted_lists.js
@@ -54,13 +54,49 @@ so each element is a list node as so:
  *     this.val = (val===undefined ? 0 : val)
  *     this.next = (next===undefined ? null : next)
  * }
+
+so, I need to identify which list to start with
+
+so the basic structure of this will be
+conditions to check if list1 and list2 have values at all
+then conditionals for if only ONE of them has values
+
+then initialize currentNode with whichever has a smaller starting node
+  then as long as list1 and list2 have values loop
+setting whichever current value is smaller to current node's next property
 */
 
 
 var mergeTwoLists = function(list1, list2) {
+  if (!list1 || !list2) {
+        return list1 ? list1 : list2;
+    }
 
+  let currentNode = list1.val <= list2.val ? list1 : list2;
+  const result = currentNode;
+
+  list1 = currentNode === list1 ? list1.next : list1;
+  list2 = currentNode === list2 ? list2.next : list2;
+
+  while (list1 || list2) {
+    if (!list1 || !list2) {
+      currentNode.next = list1 ? list1 : list2;
+      break;
+    }
+
+    if (list1.val >= list2.val) {
+      currentNode.next = list2;
+      list2 = list2.next;
+    } else {
+      currentNode.next = list1;
+      list1 = list1.next;
+    }
+
+    currentNode = currentNode.next;
+  }
+
+  return result;
 };
-
 // console.log(mergeTwoLists([1,2,4], [1,3,4])) //[1,1,2,3,4,4]
 // console.log(mergeTwoLists([], [])) //[]
 // console.log(mergeTwoLists([], [0])) //[0]
