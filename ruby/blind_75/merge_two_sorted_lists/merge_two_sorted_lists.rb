ea45fdf2a284f9fa4fcf037425dce68cd14f8cac
Lucas <lucasbonner@protonmail.com>
2022-05-24 10:07:46 -0500

    adds first attempt at merge two sorted lists in ruby

--- ruby/blind_75/merge_two_sorted_lists/merge_two_sorted_lists.rb
+++ ruby/blind_75/merge_two_sorted_lists/merge_two_sorted_lists.rb
@@ -11,9 +11,30 @@
 # @return {ListNode}
 
 def merge_two_lists(list1, list2)
+  currentNode = list1.val < list2.val ? list1 : list2
+  result = currentNode
+  list1 = currentNode == list1 ? list1.next : list1
+  list2 = currentNode == list2 ? list2.next : list2
 
+  while list1 || list2
+      if !list1 || !list2
+         currentNode.next = !list1 ? list2 : list1
+          break
+      end
+
+      if list1.val < list2.val
+         currentNode.next = list1
+          list1 = list1.next
+      else
+          currentNode.next = list2
+          list2 = list2.next
+      end
+
+      currentNode = currentNode.next
+  end
+  result
 end
 
-p merge_two_lists([1,2,4], [1,3,4]); #[1,1,2,3,4,4]
-p merge_two_lists([], []); #[]
-p merge_two_lists([], [0]); #[0]
+# p merge_two_lists([1,2,4], [1,3,4]); #[1,1,2,3,4,4]
+# p merge_two_lists([], []); #[]
+# p merge_two_lists([], [0]); #[0]
