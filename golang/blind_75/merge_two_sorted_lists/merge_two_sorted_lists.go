e7f8584b92d207ccd79d042545bc3660cebee3cb
Lucas <lucasbonner@protonmail.com>
2022-06-01 10:52:41 -0500

    adds solutions for merge_two_sorted_lists in golang

--- golang/blind_75/merge_two_sorted_lists/merge_two_sorted_lists.go
+++ golang/blind_75/merge_two_sorted_lists/merge_two_sorted_lists.go
@@ -11,10 +11,31 @@ import (
 	"fmt"
 )
 
-func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	dummy := ListNode{}
 	dummy.Next = nil
 	tail := &dummy
+
+	for l1 != nil || l2 != nil {
+		if l1 == nil {
+			(*tail).Next = l2
+			break
+		} else if l2 == nil {
+			(*tail).Next = l1
+			break
+		} else {
+			if l1.Val <= l2.Val {
+				(*tail).Next = l1
+				l1 = (*l1).Next
+			} else {
+				(*tail).Next = l2
+				l2 = (*l2).Next
+			}
+			tail = tail.Next
+		}
+	}
+
+	return dummy.Next
 }
 
 func main() {
