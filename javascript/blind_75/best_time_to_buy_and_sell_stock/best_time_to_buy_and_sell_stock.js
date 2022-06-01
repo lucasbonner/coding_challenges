9d9a95dc365e8d651a1e4c1a3f6a12898f6e9b90
Lucas <lucasbonner@protonmail.com>
2022-06-01 11:06:36 -0500

    adds whiteboarding to stock price js

---
+++ javascript/blind_75/best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.js
@@ -0,0 +1,26 @@
+/*
+instructions:
+  given array prices where prices[i] is the price of a given stock
+  on the ith day
+
+  want to maximize profit by choosing a single day to buy one stock and choosing
+  a different day in the future to sell that stock
+
+  return the maximum profit you can achieve from this transaction
+  if you cannot achieve any profit, return 0
+
+  input -> array (of stocks on given days)
+  output -> integer (representing max profit you can achieve)
+
+examples:
+given -> [7, 1, 5, 3, 6, 4]
+output -> 5
+
+*/
+
+var maxProfit = function(prices) {
+
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4])); //5
+console.log(maxProfit([7, 6, 4, 3, 1])); //0
