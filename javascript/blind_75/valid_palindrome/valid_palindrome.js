1a9237012e9cb6d068aefc8dc4f90ef925b365dc
Lucas <lucasbonner@protonmail.com>
2022-06-01 11:58:46 -0500

    adds scaffolding for valid palindrome problem

---
+++ javascript/blind_75/valid_palindrome/valid_palindrome.js
@@ -0,0 +1,18 @@
+/*
+instructions:
+a phrase is a palindrome if, after converting all uppercase letters
+into lowercase letters and removing all non-alphanumeric
+characters, it reads the same forward and backward
+alphanumeric characters include letters and numbers
+
+given a string s, return true if it is a palindrome, false otherwise
+
+*/
+
+var isPalindrome = function(s) {
+
+}
+
+console.log("A man, a plan, a canal: Panama"); //true
+console.log("race a car"); //false
+console.log(" "); //true
