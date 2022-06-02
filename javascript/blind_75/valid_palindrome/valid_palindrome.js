ce462eb2b423ba090d2d17a831b559e8544ec128
Lucas <lucasbonner@protonmail.com>
2022-06-02 11:28:45 -0500

    adds first attempt at valid palindrome

--- javascript/blind_75/valid_palindrome/valid_palindrome.js
+++ javascript/blind_75/valid_palindrome/valid_palindrome.js
@@ -7,12 +7,44 @@ alphanumeric characters include letters and numbers
 
 given a string s, return true if it is a palindrome, false otherwise
 
+  rules:
+    remove all non-alphanumeric characters from string
+    only letters an numbers
+
+    conver all uppercase to lowercase characters
+
+  input -> string
+  output -> boolean (based off palindrome check)
+
+Examples:
+"A man, a plan, a canal: Panama" -> AmanaplanacanalPanama
+AmanaplanacanalPanama <-> AmanaplanacanalPanama true
+
+"race a car" -> raceacar
+raceacar <--> racaecar false
+
+" " -> ""
+"" <-> "" true
+
+algo/pseudo
+first lowercase string and strip of non-alphanumerics
+  (either alphabet match or regex)
+split and reverse and see if equals self
+
 */
 
-var isPalindrome = function(s) {
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
+var alphanumerics = function(char) {
+  return ALPHABET.includes(char);
 }
 
-console.log("A man, a plan, a canal: Panama"); //true
-console.log("race a car"); //false
-console.log(" "); //true
+var isPalindrome = function(s) {
+  s = s.toLowerCase().split('').filter(alphanumerics).join('');
+  console.log(s);
+  return s === s.split('').reverse().join('');
+}
+
+console.log(isPalindrome("A man, a plan, a canal: Panama")); //true
+// console.log(isPalindrome("race a car")); //false
+// console.log(isPalindrome(" ")); //true
