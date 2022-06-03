20be2adbfa281aaec280220c5af72c6335dbde82
Lucas <lucasbonner@protonmail.com>
2022-06-03 10:44:03 -0500

    adds first attempt for binary search javascript

--- javascript/blind_75/binary_search/binary_search.js
+++ javascript/blind_75/binary_search/binary_search.js
@@ -17,10 +17,85 @@ instructions:
 
   input -> array of sorted nums, target int
   output -> index/-1 if not present
+
+examples:
+[-1,0,3,5,9,12], 9
+[-1,0,3,5,9,12].length === 6
+6/2 -> 3
+
+start at 3
+
+get 5, is 5 less than or greater than target?
+less than, move up an index
+find target!
+
+---------------------
+[-1,0,3,5,9,12], 2
+[-1,0,3,5,9,12].length === 6
+6/2 -> 3
+
+start at 3
+
+start at 3
+get 5, is 5 less than or greater than target?
+greater than, move left
+
+at 2
+get 3, is 3 less than or greater than target?
+greater than, move left
+
+at 1,
+get 0, is 0 less than or greater than target?
+less than, target is not present, return -1
+
+so how know that target is not present?
+if index + 1 is greater than, and index is less than, return -1
+
+so what is the looping structure
+i know i start at the middle index
+and iterate one way or the other
+based off whether the target is less than or equal to num at startIndex
+
+
 */
 
 var search = function(nums, target) {
+  let index = nums.length / 2;
+  let result = -1;
+  let comparisonElement =  nums[index];
 
+  while (comparisonElement !== target) {
+    comparisonElement = nums[index];
+
+    if (target === comparisonElement) {
+      result = index;
+      break;
+    }
+
+    if (comparisonElement > target && nums[index - 1] < target) {
+      result = -1;
+      break;
+    }
+
+    if (comparisonElement < target && nums[index + 1] > target) {
+      result = -1;
+      break;
+    }
+
+    if (comparisonElement > target) {
+      index--;
+    }
+
+    if (comparisonElement < target) {
+      index++;
+    }
+
+
+  }
+
+
+
+  return result;
 };
 
 console.log(search([-1,0,3,5,9,12], 9)); //4
