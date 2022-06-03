e715b0b5ff3419b1783ca60856e044b973034616
Lucas <lucasbonner@protonmail.com>
2022-06-03 10:09:05 -0500

    adds some pseudocode for valid anagram

---
+++ javascript/blind_75/valid_anagram/valid_anagram.js
@@ -0,0 +1,35 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the
+letters of a different word or phrase, typically using all the original
+letters exactly once.
+
+instructions:
+  given two strings, return true if they are anagrams of each other
+
+  input -> two strings
+  output -> boolean (based off whether anagrams or not)
+
+examples:
+"anagram" and "nagaram"
+
+*/
+var sortAlphabetically = function(a, b) {
+  if (a < b) {
+    return -1;
+  }
+
+  if (a > b) {
+    return 1;
+  }
+
+  return 0;
+}
+
+var isAnagram = function(s, t) {
+
+};
+
+console.log(isAnagram("anagram", "nagaram")); //true
+console.log(isAnagram("rat", "car")); //false
