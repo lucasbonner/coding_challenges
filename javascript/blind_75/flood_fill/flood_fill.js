904054044dbda6297e34b36e350755fc5c603988
Lucas <lucasbonner@protonmail.com>
2022-06-07 15:54:37 -0500

    adds another test case

--- javascript/blind_75/flood_fill/flood_fill.js
+++ javascript/blind_75/flood_fill/flood_fill.js
@@ -85,6 +85,7 @@ var floodFill = function(image, sr, sc, newColor) {
   return image;
 };
 
-console.log(floodFill([[1,1,1],[1,1,0],[1,0,1]], 1, 1, 2)); //[[2,2,2],[2,2,0],[2,0,1]]
-console.log(floodFill([[0,0,0],[0,0,0]], 0, 0, 2)); //[[2,2,2],[2,2,2]]
-console.log(floodFill([[0,0,0],[0,1,1]], 1, 1, 1));
+// console.log(floodFill([[1,1,1],[1,1,0],[1,0,1]], 1, 1, 2)); //[[2,2,2],[2,2,0],[2,0,1]]
+// console.log(floodFill([[0,0,0],[0,0,0]], 0, 0, 2)); //[[2,2,2],[2,2,2]]
+// console.log(floodFill([[0,0,0],[0,1,1]], 1, 1, 1));
+console.log(floodFill([[0,0,1],[0,0,0]], 0, 0, 2)); //[[2,2,1],[2,2,2]]
