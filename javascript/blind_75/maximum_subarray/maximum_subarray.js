c24b69d1e91af389e1a7f506a61b6c00f398739b
Lucas <lucasbonner@protonmail.com>
2022-06-07 16:31:19 -0500

    adds another test case

--- javascript/blind_75/maximum_subarray/maximum_subarray.js
+++ javascript/blind_75/maximum_subarray/maximum_subarray.js
@@ -47,3 +47,4 @@ console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])); // 6
 console.log(maxSubArray([1])); // 1
 console.log(maxSubArray([5,4,-1,7,8])); // 23
 console.log(maxSubArray([-2,1]));
+console.log(maxSubArray([8,-19,5,-4,20]));
