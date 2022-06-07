25d833c9600147ab4af55ae7e370ebcfdc8ce6cf
Lucas <lucasbonner@protonmail.com>
2022-06-07 17:01:11 -0500

    adds part of the plan for LCA

--- javascript/blind_75/lowest_common_ancestor_of_a_binary_search_tree/lowest_common_ancestor_of_a_binary_search_tree.js
+++ javascript/blind_75/lowest_common_ancestor_of_a_binary_search_tree/lowest_common_ancestor_of_a_binary_search_tree.js
@@ -12,6 +12,18 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
+
+/*
+instructions:
+  given a binary search tree (BST) find the lowest common ancestor
+  (LCA) of two given nodes in the BST
+
+  according to the definition of LCA on wikipedia
+    -> lowest common ancestor is defined between two nodes
+      p and q as the lowest node in T that has both p and q
+      as descendents
+
+*/
 var lowestCommonAncestor = function(root, p, q) {
 
 };
