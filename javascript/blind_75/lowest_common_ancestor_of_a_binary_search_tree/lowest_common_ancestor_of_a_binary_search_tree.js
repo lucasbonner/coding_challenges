e84f2126d1bd3b53a4e458bd4720134245e913a7
Lucas <lucasbonner@protonmail.com>
2022-06-07 17:12:51 -0500

    adds beginning to lca problem

--- javascript/blind_75/lowest_common_ancestor_of_a_binary_search_tree/lowest_common_ancestor_of_a_binary_search_tree.js
+++ javascript/blind_75/lowest_common_ancestor_of_a_binary_search_tree/lowest_common_ancestor_of_a_binary_search_tree.js
@@ -22,8 +22,28 @@ instructions:
     -> lowest common ancestor is defined between two nodes
       p and q as the lowest node in T that has both p and q
       as descendents
+    -> can be 'descendent' of self
+
+  input -> root, p, and q
+  output -> integer (representing the LCA of the BST)
+
+example:
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+
+so start result as root
+
+and start recursing by passing both right and left to function
+
+what is the base case here?
+  if current node equals p or q
+
+but how do I get the LCA from this?
 
 */
 var lowestCommonAncestor = function(root, p, q) {
 
+  lowestCommonAncestor(root.left, p, q);
+  lowestCommonAncestor(root.right, p, q);
 };
