8ccb47723dc0e8b520a859c0fb146b6716539baa
Lucas <lucasbonner@protonmail.com>
2022-06-09 11:04:01 -0500

    adds plan to balanced binary tree

--- javascript/blind_75/balanced_binary_tree/balanced_binary_tree.js
+++ javascript/blind_75/balanced_binary_tree/balanced_binary_tree.js
@@ -1,10 +1,3 @@
-/*
-given a binary tree, determine if it is height-balanced
-
-for this problem a height-balanced binary tree is defined as
-  -> a binary tree in which the left and right subtrees of every node differ in height
-  by no more than 1
-*/
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -17,6 +10,37 @@ for this problem a height-balanced binary tree is defined as
  * @param {TreeNode} root
  * @return {boolean}
  */
+/*
+given a binary tree, determine if it is height-balanced
+
+for this problem a height-balanced binary tree is defined as
+  -> a binary tree in which the left and right subtrees of every node differ in height
+  by no more than 1
+
+  input -> binary tree
+  output -> boolean (based off whether or not the binary tree is 'balanced')
+
+examples:
+
+[3,9,20,null,null,15,7] -> true
+
+3.left -> 9
+  (depth 1)
+3.right -> 20
+  (depth 1)
+
+20.right -> 7
+20.left -> 15
+  (depth 2)
+
+maybe an object?
+  {left: 1, right: 2}
+
+
+
+
+
+*/
 var isBalanced = function(root) {
 
 };
