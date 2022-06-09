a83766f78dea48fcbdff53fe7f9136c2fba08c9c
Lucas <lucasbonner@protonmail.com>
2022-06-09 10:59:41 -0500

    adds structure for nodes

--- javascript/blind_75/balanced_binary_tree/balanced_binary_tree.js
+++ javascript/blind_75/balanced_binary_tree/balanced_binary_tree.js
@@ -5,7 +5,18 @@ for this problem a height-balanced binary tree is defined as
   -> a binary tree in which the left and right subtrees of every node differ in height
   by no more than 1
 */
-
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
 var isBalanced = function(root) {
 
 };
