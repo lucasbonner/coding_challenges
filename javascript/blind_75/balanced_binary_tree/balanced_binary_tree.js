c1419db71e1d725b3710033ff7ec452c6d91c809
Lucas <lucasbonner@protonmail.com>
2022-06-09 10:57:55 -0500

    adds test cases

--- javascript/blind_75/balanced_binary_tree/balanced_binary_tree.js
+++ javascript/blind_75/balanced_binary_tree/balanced_binary_tree.js
@@ -5,3 +5,11 @@ for this problem a height-balanced binary tree is defined as
   -> a binary tree in which the left and right subtrees of every node differ in height
   by no more than 1
 */
+
+var isBalanced = function(root) {
+
+};
+
+console.log(isBalanced([3,9,20,null,null,15,7])); //true
+console.log(isBalanced([1,2,2,3,3,null,null,4,4]); //false
+console.log(isBalanced([]); //true
