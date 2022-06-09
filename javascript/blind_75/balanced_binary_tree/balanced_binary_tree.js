74a913b919a94fcf84af59dd24b3b1e021f5090b
Lucas <lucasbonner@protonmail.com>
2022-06-09 10:54:17 -0500

    adds balanced binary tree problem

---
+++ javascript/blind_75/balanced_binary_tree/balanced_binary_tree.js
@@ -0,0 +1,7 @@
+/*
+given a binary tree, determine if it is height-balanced
+
+for this problem a height-balanced binary tree is defined as
+  -> a binary tree in which the left and right subtrees of every node differ in height
+  by no more than 1
+*/
